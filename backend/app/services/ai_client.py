bf544e9f7f76fda80c283894027440678cf6c04c
Anukul Jena <anukul.jena@gmail.com>
2025-10-05 23:21:17 +0530

    a

--- backend/app/services/ai_client.py
+++ backend/app/services/ai_client.py
@@ -2,7 +2,7 @@ import os
 import requests
 
 class GeminiClient:
-    def __init__(self, model_name: str = "gemini-2.0-pro"):
+    def __init__(self, model_name: str = "gemini-2.0-flash"):
         self.api_key = os.getenv("GEMINI_API_KEY")
         self.model_name = model_name
         self.api_url = f"https://generativelanguage.googleapis.com/v1beta/models/{self.model_name}:generateContent"
