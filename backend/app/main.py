b89f5afe6a6cff149751b2cb0af46ca3be40ca8c
Anukul Jena <anukul.jena@gmail.com>
2025-11-02 13:34:35 +0530

    s

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,8 +2,8 @@
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.api.routes import router as api_router
-from app.api.auth import router as auth_router
+from api.routes import router as api_router
+from api.auth import router as auth_router
 
 
 app = FastAPI(
